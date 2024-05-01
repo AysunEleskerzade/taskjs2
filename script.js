bc6278a782e0a0b57e3f3eca5b0046969a75e494
Aysun Eleskerzade <ld7c3yg3h@code.edu.az>
2024-05-01 14:01:57 +0400

    task10

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
-let str2 = "Azerbaycan";
+let h1Div = document.querySelector("#h1")
 
-function isboyuk(str2) {
-    let str2Array = str2.split("");
-    if (str2Array.length > 10 && str2Array.length < 20) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-console.log(isboyuk(str2));
+let borderBtn1 = document.querySelector("#borderP")
+let borderBtn2 = document.querySelector("#borderM")
+
+let heightBtn1 = document.querySelector("#heightP")
+let heightBtn2 = document.querySelector("#heightM")
+
+let widthBtn1 = document.querySelector("#widthP")
+let widthBtn2 = document.querySelector("#widthM")
+
+let textColor = document.querySelector("#color")
+let backColor = document.querySelector("#background")
+
+let font = 18
+
+let border = 18
+borderBtn1.addEventListener("click", () => {
+    border += 5
+    h1Div.style.borderRadius = border + "px"
+
+})
+borderBtn2.addEventListener("click", () => {
+    border -= 5
+    h1Div.style.borderRadius = border + "px"
+
+})
+
+
+let height = 50
+heightBtn1.addEventListener("click", () => {
+    height += 5
+    h1Div.style.height = height + "px"
+
+})
+heightBtn2.addEventListener("click", () => {
+    height -= 5
+    h1Div.style.height = height + "px"
+
+})
+
+
+let width = 150
+widthBtn1.addEventListener("click", () => {
+    width += 5
+    h1Div.style.width = width + "px"
+
+})
+widthBtn2.addEventListener("click", () => {
+    width -= 5
+    h1Div.style.width = width + "px"
+
+})
+
+
+textColor.addEventListener("change", () => {
+    h1Div.style.color = textColor.value
+})
+
+
+backColor.addEventListener("change", () => {
+    h1Div.style.backgroundColor = backColor.value
+})
--- script1.js
+++ script1.js
@@ -1,15 +1,63 @@
-let str = "telet"
-function isPolindrom(str) {
-    let strArray = str.split("");
-    let reservedArray = [];
-    for (let i = strArray.length - 1; i >= 0; i--) {
-        reservedArray.push(strArray[i]);
-    }
-    return str === reservedArray.join("");
-}
-if (isPolindrom(str)) {
-    console.log(str + "polindromdur");
-}
-else {
-    console.log(str + "polindrom deyil");
-}
+let h1Div = document.querySelector("#h1")
+
+let borderBtn1 = document.querySelector("#borderP")
+let borderBtn2 = document.querySelector("#borderM")
+
+let heightBtn1 = document.querySelector("#heightP")
+let heightBtn2 = document.querySelector("#heightM")
+
+let widthBtn1 = document.querySelector("#widthP")
+let widthBtn2 = document.querySelector("#widthM")
+
+let textColor = document.querySelector("#color")
+let backColor = document.querySelector("#background")
+
+let font = 18
+
+let border = 18
+borderBtn1.addEventListener("click", () => {
+    border += 5
+    h1Div.style.borderRadius = border + "px"
+
+})
+borderBtn2.addEventListener("click", () => {
+    border -= 5
+    h1Div.style.borderRadius = border + "px"
+
+})
+
+
+let height = 50
+heightBtn1.addEventListener("click", () => {
+    height += 5
+    h1Div.style.height = height + "px"
+
+})
+heightBtn2.addEventListener("click", () => {
+    height -= 5
+    h1Div.style.height = height + "px"
+
+})
+
+
+let width = 150
+widthBtn1.addEventListener("click", () => {
+    width += 5
+    h1Div.style.width = width + "px"
+
+})
+widthBtn2.addEventListener("click", () => {
+    width -= 5
+    h1Div.style.width = width + "px"
+
+})
+
+
+textColor.addEventListener("change", () => {
+    h1Div.style.color = textColor.value
+})
+
+
+backColor.addEventListener("change", () => {
+    h1Div.style.backgroundColor = backColor.value
+})
